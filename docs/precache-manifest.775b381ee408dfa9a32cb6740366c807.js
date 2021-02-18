5f44a4e35770a0d73ec5b546152146699af231f8
Carlangas <gonzalezovelar97@gmail.com>
2021-02-18 18:20:28 -0300

    renombrado el build

---
+++ docs/precache-manifest.775b381ee408dfa9a32cb6740366c807.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bd95b14f84784977b19f80cc05eca45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dca57c634459fe36f6a9",
+    "url": "/static/css/main.b2d68a6c.chunk.css"
+  },
+  {
+    "revision": "a13e31d6699d5138f112",
+    "url": "/static/js/2.bed60b36.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.bed60b36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dca57c634459fe36f6a9",
+    "url": "/static/js/main.c5dbf6a1.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
